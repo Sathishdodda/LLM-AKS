d7c86921a6e1e674c456234c6a6722cbfae2b313
Sathishdodda <126811993+Sathishdodda@users.noreply.github.com>
2024-07-13 12:38:24 -0400

    Add generated Terraform code

---
+++ aks-final-05.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.91.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "aks-final-rg"
+  location = "eastus"
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-final-05"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksfinal05"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+output "cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
