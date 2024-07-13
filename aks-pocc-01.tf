67889cbe90500a5aefe4464e9d0d111c086424cf
Sathishdodda <126811993+Sathishdodda@users.noreply.github.com>
2024-07-13 10:42:45 -0400

    Add generated Terraform code

---
+++ aks-pocc-01.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.91.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "{cluster_name}_rg"
+  location = "{region}"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "{cluster_name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "{cluster_name}-dns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
+
+  tags = {
+    Environment = ""
+  }
+}
+
+output "cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
