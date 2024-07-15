f45d6c754baead6eefb89b8fa04d4153e7a0c8c5
Sathishdodda <126811993+Sathishdodda@users.noreply.github.com>
2024-07-14 22:24:11 -0400

    Add generated Terraform code

---
+++ aks-final-16.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.91.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-aks-final-16"
+  location = "East US"
+  tags = {
+    environment = "Api version 2024-03-01"
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-final-16"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksfinal16"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    load_balancer_sku = "Standard"
+    network_policy   = "azure"
+  }
+
+  tags = {
+    environment = "Api version 2024-03-01"
+  }
+}
+
+output "cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "resource_group" {
+  value = azurerm_resource_group.rg.name
+}
